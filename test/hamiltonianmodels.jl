9099f308bc0668ef446e4d9a1c1ebfc817fc45a7
XingyuZhang2018 <zhangxingyu@iphy.ac.cn>
2021-11-21 13:17:45 +0800

    fix test

--- test/hamiltonianmodels.jl
+++ test/hamiltonianmodels.jl
@@ -3,6 +3,4 @@ using ADMPS:HamiltonianModel
 
 @testset "hamiltonianmodels" begin
     @test Ising(1.0) isa HamiltonianModel
-    @test TFIsing(1.0) isa HamiltonianModel
-    @test Heisenberg() isa HamiltonianModel
 end
